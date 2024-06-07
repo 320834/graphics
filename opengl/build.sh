292fbeaa5a5ebd28d95b8f300f1590e30feef414
320834 <justinchen1000@gmail.com>
2024-06-07 13:19:18 -0400

    finish opengl setup

---
+++ opengl/build.sh
@@ -0,0 +1,17 @@
+dir_release="release"
+dir_debug="debug"
+
+# Build
+if [ ! -d "$dir_release" ]; then
+  mkdir "$dir_release" && cd "$dir_release"
+  cmake -D CMAKE_BUILD_TYPE=Release ..
+  cd ..
+fi
+
+# Debug
+if [ ! -d "$dir_debug" ]; then
+  mkdir "$dir_debug" && cd "$dir_debug"
+  cmake -D CMAKE_BUILD_TYPE=Debug ..
+  cd ..
+fi
+
