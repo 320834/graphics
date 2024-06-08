e525902496d9f7ee6dbfb80653bc027ed9eb098d
320834 <justinchen1000@gmail.com>
2024-06-08 15:23:47 -0400

    refractor some files around

---
+++ opengl/include/lessons/chp_5_hello_triangle.h
@@ -0,0 +1,12 @@
+#ifndef CHP_5_HELLO_TRIANGLE_H_
+#define CHP_5_HELLO_TRIANGLE_H_
+
+struct Initialize {
+  unsigned int shaderProgram;
+  unsigned int VAO;
+  unsigned int EBO;
+};
+
+int hello_triangle();
+
+#endif //CHP_5_HELLO_TRIANGLE_H_
