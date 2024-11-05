ff7f2cb0b4460d8185fb8376a43a4f61a5eafd74
320834 <justinchen1000@gmail.com>
2024-11-04 19:07:51 -0500

    initial setup

---
+++ engine/include/engine.h
@@ -0,0 +1,9 @@
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+template<class OpenGL>
+class Engine {
+
+};
+
+#endif // ENGINE_H_
---
+++ engine/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+  std::cout << "okay" << std::endl;
+}
